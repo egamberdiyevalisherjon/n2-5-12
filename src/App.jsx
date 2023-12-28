1f9494599d942faaa5fc2686f9cb8c23406f8a8c
Alisherjon <egamberdiyevalisherjon@gmail.com>
2023-12-28 09:39:38 +0500

    with jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import { useState } from "react";
 import Cycles from "./Components/Cycles";
 // import Hooks from "./Components/Hooks";
 
@@ -6,37 +6,37 @@ function App() {
   const [show, setShow] = useState(false);
   const [age, setAge] = useState(0);
   return (
-    // <>
-    //   <h1>Hammaga salom!</h1>
-    //   <button onClick={() => setShow(!show)}>show</button>
-    //   <button onClick={() => setAge(age + 1)}>inc</button>
-    //   {show && (
-    //     // <Hooks
-    //     //   age={32}
-    //     //   user={{
-    //     //     fName: "Eshmat",
-    //     //     lName: "toshmatov",
-    //     //     email: "eshmat@gmail.com",
-    //     //   }}
-    //     // />
-    //     <Cycles age={age} />
-    //   )}
-    // </>
+    <>
+      <h1>Hammaga salom!</h1>
+      <button onClick={() => setShow(!show)}>show</button>
+      <button onClick={() => setAge(age + 1)}>inc</button>
+      {show && (
+        // <Hooks
+        //   age={32}
+        //   user={{
+        //     fName: "Eshmat",
+        //     lName: "toshmatov",
+        //     email: "eshmat@gmail.com",
+        //   }}
+        // />
+        <Cycles age={age} />
+      )}
+    </>
 
-    React.createElement("div", null, [
-      React.createElement("h1", { key: 1 }, "Hammaga Salom!"),
-      React.createElement(
-        "button",
-        { key: 2, onClick: () => setShow(!show) },
-        "show"
-      ),
-      React.createElement(
-        "button",
-        { key: 3, onClick: () => setAge(age + 1) },
-        "inc"
-      ),
-      show && React.createElement(Cycles, { key: 4, age }),
-    ])
+    // React.createElement("div", null, [
+    //   React.createElement("h1", { key: 1 }, "Hammaga Salom!"),
+    //   React.createElement(
+    //     "button",
+    //     { key: 2, onClick: () => setShow(!show) },
+    //     "show"
+    //   ),
+    //   React.createElement(
+    //     "button",
+    //     { key: 3, onClick: () => setAge(age + 1) },
+    //     "inc"
+    //   ),
+    //   show && React.createElement(Cycles, { key: 4, age }),
+    // ])
   );
 }
 
